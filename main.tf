70098f70d244cb4e29dc419e3c4a3030866ebbb9
Mr-Catnap <140521200+Mr-Catnap@users.noreply.github.com>
2023-07-25 14:44:28 +0100

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region     = "eu-west-2"
+}
+
+resource "aws_iam_account_password_policy" "strict" {
+  minimum_password_length        = 15
+  require_uppercase_characters   = true
+  require_lowercase_characters   = true
+  require_numbers                = true
+  max_password_age               = 60
+  require_symbols                = true
+  allow_users_to_change_password = true
+  password_reuse_prevention      = 24
+}
